653231e33b766b80cd118b75ecc9695f5c42a69d
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-05-31 15:29:35 -0400

    First trait fix

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -58,6 +58,10 @@ let getWebhook = (req, res) => {
   }
 };
 
+function firstTrait(nlp, name) {
+    return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
+}
+
 function handleMessage(sender_psid, message) {
     //handle message for react, like press like button
     // id like button: sticker_id 369239263222822
