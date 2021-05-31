c7dbd3d296ef752d3d1bc37c85a7f29a71d1cadd
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-05-31 07:54:30 -0400

    Setup homepageController to render the static page in the server

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,12 @@
 import express from "express";
+import homepageController from "../controllers/homepageController"
 
 // Express Router Middleware
 let router = express.Router();
 
 
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello World!");
-    });
+    router.get("/", homepageController.getHomepage);
 
     return app.use("/", router);
 }
